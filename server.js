91affcd743b79236bd5290e102c2a4fdd6e45bbd
Logan Stewart <logan.stewart@outlook.com>
2016-03-06 09:47:35 -0700

    azure make me happy

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ http.createServer(app).listen(app.get('port'), function(){
 var express = require('express');
 var path = require('path');
 //var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -69,7 +69,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
