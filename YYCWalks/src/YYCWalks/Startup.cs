fd7115ce5b5764e9708e59e17bd0e2660de0214a
Logan Stewart <logan.stewart@outlook.com>
2016-03-19 07:26:51 -0600

    Getting it into an APS.NET5 Application and Running

---
+++ YYCWalks/src/YYCWalks/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.StaticFiles;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace YYCWalks
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles(new StaticFileOptions {
+                ServeUnknownFileTypes = true,
+                DefaultContentType = "application/vnd.google-earth.kml+xml"
+            });
+            app.UseDefaultFiles(new DefaultFilesOptions() { DefaultFileNames = new[] { "index.html", "test.kml" } });
+            
+
+
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
